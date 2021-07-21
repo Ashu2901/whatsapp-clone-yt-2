3f7bc996e038a893e048a1e909b450514e47de64
Ashu Singh <ashu012000.singh@gmail.com>
2021-07-21 17:18:30 +0530

    'modifiedImage'

--- components/Loading.js
+++ components/Loading.js
@@ -1,11 +1,12 @@
 import {Circle} from "better-react-spinkit";
+import Image from "next/image";
 
 function Loading(){
 
 
     return(
         <center style={{display:'grid',placeItems:'center' , height:'100vh'}}>
-              <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg"
+              <Image src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg"
                alt="" height={200} style={
                    {
                        marginBottom:10
